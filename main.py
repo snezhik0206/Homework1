cd2ae5fd4de6fa977e3e0f51adaaff33deb53deb
Kalasnikov Igor <snezhik0206@mail.ru>
2022-08-05 11:41:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import requests
 
 def search_hero():
     response = requests.get(url="https://akabab.github.io/superhero-api/api/all.json")
-    dictionary = response.json()
+    hero_list = response.json()
 
     heroes = ["Hulk", "Captain America", "Thanos"]
-    max_inteligence = 0
-    for hero in dictionary:
-        if hero["name"] in heroes:
-            if hero["powerstats"]["intelligence"] > max_inteligence:
-                max_inteligence = hero["powerstats"]["intelligence"]
-                max_inteligence_hero = hero["name"]
+    max_intelligence = 0
+    heroes_filter_object = filter(lambda hero: hero.get('name') in heroes, hero_list)
 
-    print(f"Самый умный герой: {max_inteligence_hero}")
+    for hero in list(heroes_filter_object):
+        if hero['powerstats']['intelligence'] > max_intelligence:
+            max_intelligence = hero['powerstats']['intelligence']
+            max_intelligence_hero = hero['name']
+
+    print(f"Самый умный герой: {max_intelligence_hero}")
 
 
 search_hero()
